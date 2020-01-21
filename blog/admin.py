2fd5ad78945e91f6f2bfe6a56504375103b5ab7c
Md Dayam Barkat <officialmddayam1999@gmail.com>
2020-01-21 17:01:38 +0530

    i am done all

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from.models import Blog
+
+admin.site.register(Blog)
+
 
 # Register your models here.
-from.models import Job
-admin.site.register(Job)
+#from.models import Job
+#admin.site.register(Job)
